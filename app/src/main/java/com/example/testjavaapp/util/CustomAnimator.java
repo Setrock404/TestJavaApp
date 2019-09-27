055da07280da042d1fdf01f4864ce84144410e17
Setrock404 <artem.maslov@globallogig.com>
2019-09-27 11:19:28 +0300

    Buttons redesigned, added string resources and notification button has different animation.

--- app/src/main/java/com/example/testjavaapp/util/CustomAnimator.java
+++ app/src/main/java/com/example/testjavaapp/util/CustomAnimator.java
@@ -46,6 +46,7 @@ public class CustomAnimator {
         AnimatorSet set = new AnimatorSet();
         set.setDuration(SHRINK_DURATION).playTogether(
                 ObjectAnimator.ofFloat(view, View.ALPHA, 1f, 0f),
+                ObjectAnimator.ofFloat(view, View.SCALE_X, 1f, 0f),
                 ObjectAnimator.ofFloat(view, View.SCALE_Y, 1f, 0f)
         );
         return set;
@@ -55,6 +56,7 @@ public class CustomAnimator {
         AnimatorSet set = new AnimatorSet();
         set.setDuration(EXPAND_DURATION).playTogether(
                 ObjectAnimator.ofFloat(view, View.ALPHA, 0f, 1f),
+                ObjectAnimator.ofFloat(view, View.SCALE_X, 0f, 1f),
                 ObjectAnimator.ofFloat(view, View.SCALE_Y, 0f, 1f)
         );
         set.setStartDelay(DELAY_DURATION);
